3b996c21ae072e2024b09fcda39c37a15fd47e6f
Taylor Knapp <taylorknapp@crosscomm.com>
2021-06-29 08:31:19 -0400

    Add iterm shell integration

---
+++ zsh/iterm.zsh
@@ -0,0 +1,6 @@
+# load iterm2 shell integration
+# https://iterm2.com/documentation-shell-integration.html
+if [ -f $HOME/.iterm2_shell_integration.zsh ];
+then
+  source $HOME/.iterm2_shell_integration.zsh;
+fi;
