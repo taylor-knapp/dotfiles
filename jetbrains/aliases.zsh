9d224989541bfa5b83fe677c383dafa84060758d
Taylor Knapp <taylorknapp@crosscomm.com>
2021-06-29 08:32:21 -0400

    Add jetbrains aliases

---
+++ jetbrains/aliases.zsh
@@ -0,0 +1,8 @@
+# Android Studio Launcher
+alias studio="open -a /Applications/Android\ Studio.app/"
+
+# IntelliJ IDEA Launcher
+alias idea="open -a /Applications/IntelliJ\ IDEA\ CE.app/"
+
+# IntelliJ RubyMine Launcher
+alias mine="open -a 'RubyMine.app'"
