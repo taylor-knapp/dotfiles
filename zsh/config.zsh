eefb4b30811eb64a7e56eff1a0c89c5226568c23
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-02 14:40:43 -0400

    Remove setopt complete_aliases, as it prevents alias expansion before searching for autocompletion options, which is the inverse of what we want 99.99% of the time.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -27,10 +27,6 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
