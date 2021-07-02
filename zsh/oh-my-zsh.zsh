44391c38e9a3c07984ae8b28810ac16242f3af40
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-02 14:30:33 -0400

    Customize oh-my-zsh theme to be a combo of robbyrussell and a longer path

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -6,7 +6,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="spaceship"
+ZSH_THEME="robbyrussell"
 
 HIST_STAMPS="mm-dd-yyyy"
 
@@ -20,4 +20,8 @@ source $ZSH/oh-my-zsh.sh
 
 source $ZSH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-export RPROMPT='%{$fg[yellow]%}[%D{%f/%m/%y} %D{%L:%M:%S}]'
+PROMPT='%{$fg[cyan]%}%30<...<%~%<<%{$reset_color%} $(git_prompt_info)'
+PROMPT+='
+%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) %{$reset_color%}'
+
+RPROMPT='%{$fg[yellow]%}[%D{%f/%m/%y} %D{%L:%M:%S}]'
