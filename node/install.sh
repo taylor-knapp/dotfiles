42b7ffe39ddcd984e11e7540e4847510177b2b88
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-06 09:09:39 -0400

    Add nvm install to bootstrapping

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,11 @@
-if test ! $(which spoof)
-then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
-fi
+export NVM_DIR="$HOME/.nvm"
+# clone if it doesn't exist
+git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+# fetch latest to ensure we're up to date if it exists already
+git fetch --tags origin
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+
+# source nvm to finish loading
+[ -s $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh  # This loads nvm
+[ -s $NVM_DIR/bash_completion ] && source $NVM_DIR/bash_completion  # This loads nvm bash_completion
