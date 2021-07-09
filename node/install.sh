d83e2809b3670b25cd66fc36756ef71f90f00c0b
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-09 08:48:15 -0400

    Clean up nvm installation
    - Use oh-my-zsh plugin to source nvm for new shells.
    - Lazy load nvm on first usage to speed up shell load time.

--- node/install.sh
+++ node/install.sh
@@ -1,18 +1,16 @@
+# Install nvm.
+
 export NVM_DIR="$HOME/.nvm"
 
-# clone if it doesn't exist
+# Clone nvm if it is not present.
 if [ ! -d $NVM_DIR ]; then
   git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR";
 fi;
 
 cd "$NVM_DIR"
 
-# fetch latest to ensure we're up to date
+# Fetch latest release to ensure it is up to date.
 git fetch --tags origin
 
-# checkout latest release
+# Checkout the latest release. When the nvm oh-my-zsh plugin sources $NVM_DIR/nvm.sh this commit will be used.
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-
-# source nvm to finish loading
-[ -s $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh  # This loads nvm
-[ -s $NVM_DIR/bash_completion ] && source $NVM_DIR/bash_completion  # This loads nvm bash_completion
--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -10,11 +10,14 @@ ZSH_THEME="robbyrussell"
 
 HIST_STAMPS="mm-dd-yyyy"
 
+# This speeds up initial zsh load time by lazy loading nvm whenever node, npm, or nvm is first used.
+NVM_LAZY=true
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git npm z)
+plugins=(git npm nvm z)
 
 source $ZSH/oh-my-zsh.sh
 
