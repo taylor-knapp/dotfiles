1d0e95076578fc7e6cf085779366f2048004ad75
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-09 08:46:03 -0400

    Use oh-my-zsh z plugin instead of custom installation

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -14,7 +14,7 @@ HIST_STAMPS="mm-dd-yyyy"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git npm)
+plugins=(git npm z)
 
 source $ZSH/oh-my-zsh.sh
 
--- zsh/z.zsh
+++
@@ -1,3 +0,0 @@
-# Load z jump around command for quick terminal navigation
-# https://github.com/rupa/z
-source `brew --prefix`/etc/profile.d/z.sh
