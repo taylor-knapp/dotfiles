1c202fb5f40fe3f5315c38302787162a60ba1602
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-13 08:49:30 -0400

    Add sdkman autocompletion

---
+++ java/install.sh
@@ -0,0 +1,10 @@
+# Set the skdman directory.
+# This is the default, but the path is used elsewhere so force it to ensure it never varies.
+SDKMAN_DIR="$HOME/.sdkman"
+
+# Install sdkman, or update if it already is installed.
+if [ ! -d $SDKMAN_DIR ]; then
+  curl -s "https://get.sdkman.io?rcupdate=false" | bash
+else
+  sdk selfupdate force
+fi
