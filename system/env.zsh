df71b7dda9f7f92c6d47a92417d58974cbed105a
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-18 09:37:00 -0400

    Add less defaults

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,8 @@
 export EDITOR='storm'
+
+# Set defaults for using `less`.
+# -M displays lines and percentage (once the full buffer has been loaded).
+# +Gg adds initial commands to execute. G loads to the end of the file, and g jumps back to the beginning.
+#   This trick always shows the total lines and percentage while scrolling, but can slowdown loads for large buffers.
+#   To deal with slow load times, you can change the initial commands to just start at the beginning with `less +g.
+export LESS='-M +Gg'
