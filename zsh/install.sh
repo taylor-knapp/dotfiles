018785cca70292e287231043f7aefbaadb9137fd
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2021-07-18 20:05:48 -0400

    Add powerlevel10k

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,9 +4,16 @@ rm -rf $ZSH
 # Install oh-my-zsh. Do not replace the existing .zshrc file. Do not start zsh after the install.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
 
+ZSH_CUSTOM=$ZSH/custom
+
 # Fix slow pasting with this odd trick.
 # https://www.reddit.com/r/zsh/comments/bj6rwz/what_is_a_good_ohmyzsh_alternative/em7l131?utm_source=share&utm_medium=web2x&context=3
 mkdir -p $ZSH_CUSTOM/lib && touch $ZSH_CUSTOM/lib/misc.zsh
+
+# Setup powerlevel10k theme.
+# https://github.com/romkatv/powerlevel10k/#meslo-nerd-font-patched-for-powerlevel10k
+rm -rf $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 # Install syntax highlighting support.
 brew install zsh-syntax-highlighting
--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -3,10 +3,7 @@ echo 'loading oh-my-zsh'
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 HIST_STAMPS="mm-dd-yyyy"
 
@@ -21,8 +18,8 @@ plugins=(git npm nvm z)
 
 source $ZSH/oh-my-zsh.sh
 
-PROMPT='%{$fg[cyan]%}%30<...<%~%<<%{$reset_color%} $(git_prompt_info)'
-PROMPT+='
-%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) %{$reset_color%}'
-
-RPROMPT='%{$fg[yellow]%}[%D{%f/%m/%y} %D{%L:%M:%S}]'
+#PROMPT='%{$fg[cyan]%}%30<...<%~%<<%{$reset_color%} $(git_prompt_info)'
+#PROMPT+='
+#%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) %{$reset_color%}'
+#
+#RPROMPT='%{$fg[yellow]%}[%D{%f/%m/%y} %D{%L:%M:%S}]'
