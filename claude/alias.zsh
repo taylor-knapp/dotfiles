3a10986bfae2abbbc1d0c05bb5f0fce683bb1725
Taylor Knapp <22882623+taylor-knapp@users.noreply.github.com>
2026-02-17 08:08:11 -0500

    chore: update cl alias

--- claude/alias.zsh
+++ claude/alias.zsh
@@ -1,2 +1,2 @@
 # Set the task list id and pass through arguments to claude.
-function cl { node -v > /dev/null 2>&1; CLAUDE_CODE_TASK_LIST_ID=$(git rev-parse --path-format=absolute --git-common-dir) claude "$@"; }
+alias cl=claude
